7e7fb278e52b21bff5185feb56726fafc1c3358d
Aidar Rakhamtullin <mr.darik88@gmail.com>
2020-12-02 22:55:17 +0300

    Initial Commit

---
+++ PhotoTapps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by Айдар Рахматуллин on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
